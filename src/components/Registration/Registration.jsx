09bc727fec40fa293fbc0d0c799224a1124e7a3a
Dmitriy Prokoshev <pda1990@mail.ru>
2022-02-13 21:48:47 +0300

    Update Registration.jsx

--- src/components/Registration/Registration.jsx
+++ src/components/Registration/Registration.jsx
@@ -1,42 +1,11 @@
-import React from "react";
-import Form from "../Form";
+import React from 'react';
+import Form from '../Form';
+import { fields } from './constants';
 
 const Registration = () => {
-
-  const fields = [
-    {
-      type: 'text',
-      name: 'name',
-      placeholder: 'Имя',
-      icon: 'user'
-    },
-    {
-      type: 'text',
-      name: 'surname',
-      placeholder: 'Фамилия',
-      icon: 'user'
-    },
-    {
-      type: 'email',
-      name: 'email',
-      placeholder: 'Email',
-      icon: 'email'
-    },
-    {
-      type: 'password',
-      name: 'password',
-      placeholder: 'Пароль',
-      icon: 'pass'
-    },
-    {
-      type: 'password',
-      name: 'repeat_password',
-      placeholder: 'Повторите пароль',
-      icon: 'pass'
-    }
-  ];
-
-  return <Form title='Регистрация' fields={fields} textButton='Зарегистрироваться' />
-}
+  return (
+    <Form title="Регистрация" fields={fields} textButton="Зарегистрироваться" />
+  );
+};
 
 export default Registration;
